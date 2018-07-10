70ab406ffd50686af4e112538a4a5adea8a9a9a8
sibyldawn <sibyldawn@gmail.com>
2018-07-10 16:31:03 -0700

    Complete Tasks

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const checkForSession = require('./middlewares/checkForSession');
+const swag_controller = require('./controllers/swag_controller');
+const auth_controller = require('./controllers/auth_controller');
+const cart_controller = require('./controllers/cart_controller');
+const search_controller = require('./controllers/search_controller');
+
+require('dotenv').config();
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(checkForSession);
+app.use(express.static(`${__dirname}/build`));
+
+
+/*SWAG_CONTROLLER */
+app.get('/api/swag', swag_controller.read);
+
+/* AUTH_CONTROLLER ENDPOINTS */
+app.post('/api/login', auth_controller.login);
+app.post('/api/register', auth_controller.register);
+app.post('/api/signout',auth_controller.signout);
+app.get('/api/user', auth_controller.getUser);
+
+/* CART_CONTROLLER ENDPOINTS */
+app.post('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout',cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete);
+
+/* SEARCH_CONTROLLER ENDPOINTS */
+app.get('/api/search',search_controller.search);
+
+
+
+const PORT = 3000;
+app.listen(PORT, ()=>{console.log(`Server listening on port ${PORT}.`);});
